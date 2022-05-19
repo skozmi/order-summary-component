6042c82177f4f85d9eb9e7ecfa8b3052c4f082aa
skozmi <skozmi@gmail.com>
2022-05-19 09:48:37 +0200

    finished html and express

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.get('/', (req, res, next)=>{
     res.sendFile(__dirname + '/index.html');
 });
 
+app.post('/payment', (req, res, next)=>{
+    res.send('<script>alert("Thank you for payment!"); window.location.href = "/"; </script>')
+});
+
+app.post('/cancel', (req, res, next)=>{
+    res.send('<script>alert("Canceled order!"); window.location.href = "/"; </script>')
+});
+
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}.`);
 });
